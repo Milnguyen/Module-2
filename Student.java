d2b69e59b419b1889bbfc0ba0f6d577c1c27ff40
Milnguyen <nguyenhang97hy@gmail.com>
2025-12-24 18:49:54 +0900

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,21 +1,17 @@
 public class Student {
-    private int rollno;
-    private String name;
-    private static String college = "BBDIT";
+    private String name = "John";
+    private String classes = "C02";
 
-    //constructor to initialize the variable
-    Student(int r, String n) {
-        rollno = r;
-        name = n;
+    // Constructor không tham số
+    public Student() {
     }
 
-    //static method to change the value of static variable
-    static void change() {
-        college = "CODEGYM";
+    // Setter
+     void setName(String name) {
+        this.name = name;
     }
 
-    //method to display values
-    void display() {
-        System.out.println(rollno + " " + name + " " + college);
+     void setClasses(String classes) {
+        this.classes = classes;
     }
 }
